fdb5f09107ccd2ee060ee9e6da2ad6fc08f4f6b0
Bryan Lown <63948652+lowner75@users.noreply.github.com>
2022-12-13 10:39:21 +0000

    Update movingAverage.js

--- movingAverage.js
+++ movingAverage.js
@@ -1,16 +1,20 @@
+// A simple moving average calculator which returns an array of 
+// averages based upon inputted data and a specified time frame ...
+
 /**
 *
-* @param {array} 	data       // the data used to calculate the moving average
-* @param {number} timeFrame  // moving average length
+* @param {array}  data        // the data inputted to base the moving average on - in this instance: monthly sales
+* @param {number} timeFrame   // moving average length - in this instance: number of months
 *
 */
 
 // Variables
+// 24 months of sales data ...
 const data = [
   40000, 50000, 80000, 50000, 35000, 70000, 55000, 70000, 95000, 72500, 90000, 50000,
   70000, 65000, 80000, 60000, 85000, 62500, 60000, 95000, 90000, 87500, 115000, 85000
 ];
-const timeFrame = 3;
+const timeFrame = 3; // number of months, meaning this will return an array of 3 month moving averages
 
 const calculateMovingAverage = (data, timeFrame) => {
 	
@@ -18,8 +22,10 @@ const calculateMovingAverage = (data, timeFrame) => {
 	let total = 0;
 	let count = 0;
 
+	// Iterate sales data
 	for (let i = 0; i < data.length; i++) {
 		
+		// Iterate the number of months back for the specified time frame from the current month's data
 		for (let j = 0; j < timeFrame; j++) {
 			if (data[i-j] !== undefined) {
 				total += data[i-j];
@@ -29,7 +35,7 @@ const calculateMovingAverage = (data, timeFrame) => {
 		
 		const currentMonthAverage = total / count;
 		movingAverage[i] = parseInt(currentMonthAverage); // No need to parse if you don't need to ... 
-    total = 0; count = 0;
+		total = 0; count = 0;
 
 	};
 	
